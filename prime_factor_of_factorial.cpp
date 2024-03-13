79625e6eee0d2782306a5d0523490ad8cf3bca36
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2024-03-13 11:05:07 +0600

    Update prime_factor_of_factorial.cpp

--- prime_factor_of_factorial.cpp
+++ prime_factor_of_factorial.cpp
@@ -53,3 +53,61 @@ int32_t main()
     prime_fact_of_factorial(n);
     return 0;
 }
+
+
+
+///More efficient
+//#include<bits/stdc++.h>
+//
+//using namespace std;
+//
+///// complexity O (n log( log(n ) ) )
+//const int N= 1e6+9;
+//bitset<N>prime;
+//vector<int>prm;
+//void sieve()
+//{
+//    for(int i =1;i<N;i++)
+//        prime[i]=true;
+//    prime[1]=false;
+//    for(int i = 2;i*i<N;i++)
+//    {
+//        if(prime[i])
+//        {
+//            for(int j = i*i;j<N;j+=i)
+//                prime[j]=false;
+//        }
+//    }
+//    for(int i =2 ;i<N;i++)
+//        if(prime[i])
+//          prm.push_back(i);
+//}
+//void prime_fact_of_factorial(int n)
+//{
+//    for(auto p : prm)
+//    {
+//        if(p> n)
+//            break;
+//        int e =0;
+//        for(int i = p ;i <= n;i += p)
+//        {
+//            int temp = i;
+//            while(temp % p ==0)
+//            {
+//                ++e;
+//                temp/=p;
+//            }
+//        }
+//        if(e)
+//        cout<<"prime "<<p<<"^"<<e<<endl;
+//    }
+//}
+//
+//int32_t main()
+//{
+//    int n;
+//    cin>>n;
+//    sieve();
+//    prime_fact_of_factorial(n);
+//    return 0;
+//}
